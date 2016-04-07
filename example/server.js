e29b792d430b5ca12cedb1d66677f37e360a09fe
Yu Wang <opensource@undozen.com>
2016-04-07 16:38:00 +0800

    update example/

--- example/server.js
+++ example/server.js
@@ -2,7 +2,4 @@
 var config = require('./config');
 var Server = require('simple-npm-cache-proxy/server');
 var server = Server(config);
-var Updater = require('simple-npm-cache-proxy/updater');
-var updater = Updater(config);
 server.listen(7000);
-updater.run();
--- example/updater.js
+++ example/updater.js
@@ -1,8 +1,5 @@
 'use strict';
 var config = require('./config');
-var Server = require('simple-npm-cache-proxy/server');
-var server = Server(config);
 var Updater = require('simple-npm-cache-proxy/updater');
 var updater = Updater(config);
-server.listen(7000);
 updater.run();
